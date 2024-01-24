5f3ef4e8b8ea23d96ff92457d4015d280a2ec103
Bonevy BEBY <bonevybeby@gmail.com>
2024-01-24 15:55:11 +0100

    add and display skills

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,82 @@
 import React, {Component} from 'react'
 
 export class About extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            title:"Curriculum vitae developped with React js",
+            contact:{name:"Bonevy BEBY",profile:"images/profile.jpg",email:"bonevybeby@gmail.com"},
+            skills:[
+                {id:1,skill:"Software engineering"},
+                {id:2,skill:"Cloud Computing"},
+                {id:3,skill:"Machine and Deep Learning"},
+            ],
+            skillValue:'default value',
+        }
+    }
+    setSkill= (event)=> {
+        this.setState({
+            skillValue: event.target.value
+        });
+    }
+    addSkill= (event)=> {
+        event.preventDefault();
+        let new_skill={
+        id:[...this.state.skills].pop().id+1,
+        skill:this.state.skillValue
+    }
+    this.setState({
+        skills:[...this.state.skills,new_skill]
+    });
+
+    }
     render() {
         return (
             <div>
-                <h3>About Component</h3>
+                <div className="card">
+                    <div className="card-header">
+                        <strong><label>{this.state.title}</label></strong>
+                    </div>
+                    <div className="row p-2">
+                        <div className="col col-auto">
+                            <img src={this.state.contact.profile} width={100}/>
+                        </div>
+                        <div className="col">
+                            <ul className="list-group">
+                                <li className="list-group-item">{this.state.contact.name}</li>
+                                <li className="list-group-item">{this.state.contact.email}</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="card m-2">
+                   <div className="card-header">Skills:{this.state.skillValue}</div>
+                    <div className="card-body">
+                            <form onSubmit={this.addSkill}>
+                                <div className="row  g-2">
+                                  <div className="col col-auto">
+                                      <input type="text" name="skill" placeholder="New Skill" value={this.state.skillValue}
+                                      onChange={this.setSkill}/>
+                                  </div>
+                                    <div className="col col-auto">
+                                        <button className="btn btn-primary btn-sm" type="submit">add</button>
+                                    </div>
+                                </div>
+                            </form>
+                        <table className="table">
+                            <tr>
+                                <th>ID</th><th>Skill</th>
+                            </tr>
+                            {
+                                this.state.skills.map((s,index)=>
+                                 <tr>
+                                     <td>{s.id}</td> <td>{s.skill}</td>
+                                 </tr>
+                                )
+                            }
+                        </table>
+                    </div>
+                </div>
             </div>
         )
     }
